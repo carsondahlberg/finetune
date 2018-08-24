5621b375f8be2394f090657645009e5090c97f51
Madison May <madison@indico.io>
2018-08-24 16:24:04 -0400

    WIP: class_weights

--- finetune/datasets/stanford_sentiment_treebank.py
+++ finetune/datasets/stanford_sentiment_treebank.py
@@ -40,8 +40,8 @@ class StanfordSentimentTreebank(Dataset):
 
 if __name__ == "__main__":
     # Train and evaluate on SST
-    dataset = StanfordSentimentTreebank(nrows=1000).dataframe
-    model = Classifier(verbose=True, n_epochs=2, val_size=0.01, val_interval=10, visible_gpus=[])
+    dataset = StanfordSentimentTreebank(nrows=100).dataframe
+    model = Classifier(verbose=True, n_epochs=2, val_size=0.01, val_interval=10, batch_size=8, low_memory_mode=True, class_weights={'0': 1, '1': 100})
     trainX, testX, trainY, testY = train_test_split(dataset.Text, dataset.Target, test_size=0.3, random_state=42)
     model.fit(trainX, trainY)
     accuracy = np.mean(model.predict(testX) == testY)
